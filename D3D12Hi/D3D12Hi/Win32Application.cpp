ae28b4af456c3ac62fa9df1821a87feeb9123e71
Leusin <leusin@naver.com>
2024-04-14 17:07:40 +0900

    Complete Sin32Application class

---
+++ D3D12Hi/D3D12Hi/Win32Application.cpp
@@ -0,0 +1,108 @@
+#include "Win32Application.h"
+
+#include "DXSample.h"
+
+HWND Win32Application::m_hwnd = nullptr;
+
+int Win32Application::Run(DXSample* pSample, HINSTANCE hInstance, int nCmdShow)
+{
+	// 커맨드라인(cl) 매개 변수 파싱
+	int argc;
+	LPWSTR* argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+	pSample->ParseCommandLineArgs(argv, argc);
+	LocalFree(argv);
+
+	// 윈도우 클래스 초기화
+	WNDCLASSEX windowClass = { 0 };
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = WindowProc;
+	windowClass.hInstance = hInstance;
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.lpszClassName = L"DXSampleClass";
+	RegisterClassEx(&windowClass);
+
+	RECT windowRect = { 0, 0, static_cast<LONG>(pSample->GetWidth()), static_cast<LONG>(pSample->GetHeight()) };
+	AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+	// 윈도우와 생성 및 생성한 윈도우를 핸들에 저장
+	m_hwnd = CreateWindow(
+		windowClass.lpszClassName,
+		pSample->GetTitle(),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		windowRect.right - windowRect.left,
+		windowRect.bottom - windowRect.top,
+		nullptr,        // We have no parent window.
+		nullptr,        // We aren't using menus.
+		hInstance,
+		pSample);
+
+	// 샘플 초기화. OnInit 은 DXSample의 파생 클래스 구현에서 정의된다.
+	pSample->OnInit();
+
+	ShowWindow(m_hwnd, nCmdShow);
+
+	// 메인 샘플 루프.
+	MSG msg = {};
+	while (msg.message != WM_QUIT)
+	{
+		// 큐 안의 메시지를 처리.
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	pSample->OnDestroy();
+
+	// WM_QUIT 메시지의 일부를 Windows에게 반환.
+	return static_cast<char>(msg.wParam);
+}
+
+LRESULT Win32Application::WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	DXSample* pSample = reinterpret_cast<DXSample*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+
+	switch (message)
+	{
+	case WM_CREATE:
+	{
+		// CreateWindow에 전달된 DXSample*
+		LPCREATESTRUCT pCreateStruct = reinterpret_cast<LPCREATESTRUCT>(lParam);
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pCreateStruct->lpCreateParams));
+	}
+	return 0;
+
+	case WM_KEYDOWN:
+		if (pSample)
+		{
+			pSample->OnKeyDown(static_cast<UINT8>(wParam));
+		}
+		return 0;
+
+	case WM_KEYUP:
+		if (pSample)
+		{
+			pSample->OnKeyUp(static_cast<UINT8>(wParam));
+		}
+		return 0;
+
+	case WM_PAINT:
+		if (pSample)
+		{
+			pSample->OnUpdate();
+			pSample->OnRender();
+		}
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	// switch 문에서 처리되지 않은 모든 메시지를 처리.
+	return DefWindowProc(hWnd, message, wParam, lParam);
+}
--- D3D12Hi/D3D12Hi/Win32Application.h
+++ D3D12Hi/D3D12Hi/Win32Application.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <windows.h>
 
+class DXSample;
+
+/// <summary>
+/// 변수와 메서드가 모두 정적(static)으로 선언되어 있기 때문에 
+/// 인스턴스를 생성하지 않고도 접근할 수 있다.
+/// </summary>
 class Win32Application
 {
 public:
+	static int Run(DXSample* pSample, HINSTANCE hInstance, int nCmdShow);
 	static HWND GetHwnd() { return m_hwnd; }
+
+protected:
+	static LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 private:
 	static HWND m_hwnd;
